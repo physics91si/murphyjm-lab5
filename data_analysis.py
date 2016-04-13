06022d92d1eb62ca1bcf0f3ad2f57b4c897bbc3e
Joey Murphy <murphyjm@stanford.edu>
2016-04-12 18:21:30 -0700

    Almost done with lab5. Need to find a way to zero out high frequency components to clean the y-values in data_analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+N = 200
+max = 20
+timestep = max/N
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
@@ -14,7 +18,16 @@ def main():
     """This function sould call noisy_packet() to get a Gaussian wave
     packet, call clean_data() to apply a low pass filter to the data and
     finally plot the result."""
-    #TODO add your code here
+    x = np.linspace(-max, max, N)
+    y = noisy_packet(x, 5, 1, 2)
+
+    y_clean = clean_data(x, y)
+
+    plt.plot(x, y)
+    plt.show()
+
+    plt.plot(x, y_clean)
+    plt.show()
 
 
 def noisy_packet(x_values, k, sigma, noise_amplitude):
@@ -29,8 +42,10 @@ def clean_data(x_values,y_values):
     """This function should take a set of y_values, perform the Fourier
     transform on it, filter out the high frequency noise, transform the
     signal back into real space, and return it."""
-
-    pass #TODO add your code here
-    #return y_clean
+    # xf = np.fft.fftfreq(N, d=timestep)
+    yf = np.fft.fft(y_values)
+    # Need to find a way how to zero out noisy high frequency components
+    y_clean = np.fft.irfft(yf, N)
+    return y_clean
 
 main()  # calls your main function
--- trig.py
+++ trig.py
@@ -3,11 +3,26 @@
 #The following statements are used to import numpy and matplotlib.
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
+x = np.arange(0, np.pi, 0.1)
+y = np.sin(x)
 
 # TODO fill in this function
 def integrate(y, dx):
-    pass
+	return np.sum(y*dx)
 
-# TODO write code here to setup arrays x and y = sin(x) and then plot them.
-# After this is done implement your integrate function above integrate y
+print('Numercial approximation of integral of sin(x) from 0 to pi = %f \n') %integrate(y, 0.1)
+
+plt.plot(x, y)
+plt.show()
+
+y_1 = np.cos(x)
+
+I = quad(lambda x: np.cos(x), 0, np.pi)
+I = I[0]
+print('Numerical approximation of integral of cos(x) from 0 to pi = %f \n') %I
+
+plt.figure()
+plt.plot(x, y_1)
+plt.show()
